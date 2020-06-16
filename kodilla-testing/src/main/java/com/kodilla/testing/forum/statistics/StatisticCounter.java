2b124c89885a2020f5ee849ba269b20eed6c5a29
justynaWoj <j.wojciechowska2011@gmail.com>
2020-06-16 22:34:18 +0200

    first commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticCounter.java
@@ -0,0 +1,30 @@
+package com.kodilla.testing.forum.statistics;
+
+import java.util.List;
+
+public class StatisticCounter {
+    private int usersNumber;
+    private int totalPosts;
+    private int totalComments;
+    private double usersAveragePostsQuantity;
+    private double usersAverageCommentsQuantity;
+    private double postsAverageCommentsQuantity;
+
+    public void calculateAdvStatistics(Statistics statistics){
+        this.usersNumber = statistics.usersNames().size();
+        this.totalPosts = statistics.postsCount();
+        this.totalComments = statistics.commentsCount();
+        if(totalPosts != 0) {
+            this.usersAveragePostsQuantity = usersNumber / totalPosts;
+        }
+        if (totalComments != 0) {
+            this.usersAverageCommentsQuantity = usersNumber / totalComments;
+            this.postsAverageCommentsQuantity = totalPosts / totalComments;
+           }
+        }
+
+
+    public String showStatistics(){
+        return "usersNumber "+usersNumber+"; totalComments "+ totalComments+", totalPosts "+totalPosts+ " usersAveragePostsQuantity "+usersAveragePostsQuantity+ ", usersAverageCommentsQuantity "+usersAverageCommentsQuantity+", postsAverageCommentsQuantity "+postsAverageCommentsQuantity;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticCounterTest.java
@@ -0,0 +1,37 @@
+package com.kodilla.testing.forum.statistics;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+
+public class StatisticCounterTest {
+
+    @Test
+    public void calculateAdvStatisticsTest() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        StatisticCounter statisticCounter = new StatisticCounter();
+
+    }
+
+/*
+*
+* gdy liczba postów = 0,
+gdy liczba postów = 1000,
+gdy liczba komentarzy = 0,
+gdy liczba komentarzy < liczba postów,
+gdy liczba komentarzy > liczba postów,
+gdy liczba użytkowników = 0,
+gdy liczba użytkowników = 100.
+*
+
+*     List<String> usersNames(); // list of users names
+    int postsCount();          // total quantity of forum posts
+    int commentsCount();       // total quantity of forum comments
+ */
+
+    //Given
+    //When
+    //Then
+}
