2eac5dff26fdd48846f0b24710535f719269a9cb
justynaWoj <j.wojciechowska2011@gmail.com>
2020-06-21 13:22:29 +0200

    changed .filter to calculate if current age is greater than 20

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -12,12 +13,13 @@ public class StreamMain {
         Forum forum = new Forum();
         Map<Integer,ForumUser> filteredUsers = forum.getUsersList().stream()
                 .filter(user -> user.getSex()=='M')
-                .filter(user -> user.getDateOfBirth().getYear()<2000)
+                .filter(user -> user.getDateOfBirth().isBefore(LocalDate.now().minusYears(20)))
                 .filter(user -> user.getPostsQuantity()>0)
                 .collect(Collectors.toMap(forumUser -> forumUser.getIdNumber(),user ->user));
 
         filteredUsers.entrySet().stream()
                 .map(entry -> entry.getKey() +" "+ entry.getValue())
                 .forEach(System.out::println);
+
     }
 }
