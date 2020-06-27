ef3721be6f67f12f928f7cf97a3e860c46b6b681
justynaWoj <j.wojciechowska2011@gmail.com>
2020-06-27 18:03:46 +0200

    8.3 exercise

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,12 +1,33 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+import com.kodilla.exception.test.ExceptionHandling;
+import com.kodilla.exception.test.SecondChallenge;
+
 
 public class ExceptionModuleRunner {
     public static void main(String[] args) {
 
+        FileReaderWithoutHandling fileReaderWithoutHandling = new FileReaderWithoutHandling();
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+
+        try{
+            fileReader.readFile();
+        } catch (FileReaderException e){
+            System.out.println("Problem while reading file!");
+        }
+
+        //Second challenge
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try{
+            System.out.println(secondChallenge.probablyIWillThrowException(5,5));
+        } catch (ExceptionHandling e){
+            System.out.println("Wrong x and/or y value");
+        }
+
 
     }
 }
