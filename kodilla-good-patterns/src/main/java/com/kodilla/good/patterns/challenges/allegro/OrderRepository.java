0e738b495ccda9e22b20c69640c32b5e2367d277
justynaWoj <j.wojciechowska2011@gmail.com>
2020-08-12 11:12:23 +0200

    change OrderRepository class to interface

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/allegro/OrderRepository.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/allegro/OrderRepository.java
@@ -3,11 +3,11 @@ package com.kodilla.good.patterns.challenges.allegro;
 import java.util.HashMap;
 import java.util.Map;
 
-public class OrderRepository {
+public interface OrderRepository {
 
     Map<Integer, OrderRequest> ordersHistory = new HashMap<>();
 
-    public void saveOrderInHistory(OrderRequest orderRequest){
+    default void saveOrderInHistory(OrderRequest orderRequest){
         int orderNumber = orderRequest.getOrderNumber();
         ordersHistory.put(orderNumber,orderRequest);
     }
