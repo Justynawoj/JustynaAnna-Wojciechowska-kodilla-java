67af7d5635e3c08a8a0da33558f2695f92ef4f6d
justynaWoj <j.wojciechowska2011@gmail.com>
2020-08-12 09:43:29 +0200

    package created

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/allegro/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/allegro/Main.java
@@ -1,4 +1,4 @@
-package com.kodilla.good.patterns.challenges;
+package com.kodilla.good.patterns.challenges.allegro;
 
 class Main{
     public static void main(String[] args) {
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/allegro/OrderRepository.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/allegro/OrderRepository.java
@@ -1,7 +1,6 @@
-package com.kodilla.good.patterns.challenges;
+package com.kodilla.good.patterns.challenges.allegro;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class OrderRepository {
