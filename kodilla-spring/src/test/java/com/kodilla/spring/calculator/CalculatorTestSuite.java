6930d68ad5f0e27331376a021d886190b175dc34
justynaWoj <j.wojciechowska2011@gmail.com>
2020-08-14 15:09:42 +0200

    14.2 poprawione

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -12,25 +12,43 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CalculatorTestSuite {
 
+    //Given
+    ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+    //  Calculator calculator = context.getBean(Calculator.class);
+    Calculator calculator = (Calculator) context.getBean("calculator");
 
     @Test
-    public void testCalculations() {
-
-        //Given
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        //  Calculator calculator = context.getBean(Calculator.class);
-        Calculator calculator = (Calculator) context.getBean("calculator");
+    public void testCalculationsAddMethod() {
 
         //When
-        double addResult = calculator.add(5,4);
-        double subResult = calculator.sub(5,4);
-        double mulResult = calculator.mul(5,4);
-        double divResult = calculator.div(5,5);
-
+        double addResult = calculator.add(5, 4);
         //Then
-        Assert.assertEquals(addResult,9,1);
-        Assert.assertEquals(subResult,1,1);
-        Assert.assertEquals(mulResult,20,1);
-        Assert.assertEquals(divResult,1,1);
+        Assert.assertEquals(addResult, 9, 1);
+    }
+
+    public void testCalculationsSubMethod() {
+
+        //When
+        double subResult = calculator.sub(5, 4);
+        //Then
+        Assert.assertEquals(subResult, 1, 1);
+    }
+
+    public void testCalculationsMulMethod() {
+        //When
+        double mulResult = calculator.mul(5, 4);
+        //Then
+        Assert.assertEquals(mulResult, 20, 1);
+    }
+
+    public void testCalculationsDivMethod() {
+        //When
+        double divResult = calculator.div(5, 5);
+        //Then
+        Assert.assertEquals(divResult, 1, 1);
     }
 }
+
+
+
+
