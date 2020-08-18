d69796692bf15fb4c7860c548cfc0d3f673f1a0d
justynaWoj <j.wojciechowska2011@gmail.com>
2020-08-18 11:11:42 +0200

    15.3

--- kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/publisher/FacebookPublisher.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/publisher/FacebookPublisher.java
@@ -5,22 +5,7 @@ import com.kodilla.patterns.strategy.social.SocialPublisher;
 public class FacebookPublisher implements SocialPublisher {
     @Override
     public String showSocial() {
-
         System.out.println("Facebook");
         return "Facebook";
     }
 }
-
-  /*  W katalogu src/main/java utwórz pakiet com.kodilla.patterns.strategy.social.
-        Utwórz klasę User, zawierającą nazwę użytkownika.
-        Utwórz dziedziczące z niej klasy Millenials, YGeneration oraz ZGeneration.
-        Stwórz interfejs SocialPublisher, zawierający metodę share().
-        Stwórz implementujące go klasy FacebookPublisher, TwitterPublisher oraz SnapchatPublisher
-        — niech wyświetlają nazwę medium społecznego, którego używają.
-        Zastosuj wzorzec Strategy do klas User oraz klas dziedziczących z tej klasy:
-        w konstruktorze ustaw dla danej kategorii użytkownika odpowiednie medium społecznościowe,
-        a w klasie User dodaj metodę sharePost().
-        W katalogu src/test/java stwórz pakiet com.kodilla.patterns.strategy.social.
-        Utwórz zestaw testów UserTestSuite, a w nim dwa testy: testDefaultSharingStrategies()
-        oraz testIndividualSharingStrategy() sprawdzające, czy domyślnie utworzone obiekty generujące wpisy
-        w mediach społecznościowych są poprawne, a także, czy możliwa jest indywidualna zmiana upodobań użytkownika.*/
