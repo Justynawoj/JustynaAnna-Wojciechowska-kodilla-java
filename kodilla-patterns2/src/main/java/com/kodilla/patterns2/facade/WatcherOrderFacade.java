03030ae5b70435a8b46a36de8ea55dff1067a2e0
justynaWoj <j.wojciechowska2011@gmail.com>
2021-01-01 20:37:50 +0100

    24.3

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/WatcherOrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/WatcherOrderFacade.java
@@ -1,7 +1,5 @@
 package com.kodilla.patterns2.facade;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -17,30 +15,9 @@ public class WatcherOrderFacade {
     private final static List<String> logs = new ArrayList<>();
     private static final Logger LOGGER = LoggerFactory.getLogger(WatcherOrderFacade.class);
 
-
     @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
     public void logEvent() {
         LOGGER.info("ProcessOrder method was executed");
-    }
-    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
-    public Object saveLogEvent(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        Object result;
-
-        try {
-            result = proceedingJoinPoint.proceed();
-            LOGGER.info("Logging around the event");
-
-        } catch (Throwable throwable){
-            logs.add(throwable.getMessage());
-            throw throwable;
-        }
-        finally {
-            System.out.println("The saved logs: ");
-            for (String log:logs) {
-                System.out.println(log);
-            }
-        }
-        return result;
     }
 
 }
