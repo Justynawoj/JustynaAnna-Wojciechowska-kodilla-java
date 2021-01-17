bb3269b96672d5815bdd20a4fcbf3c85226c8a2c
justynaWoj <j.wojciechowska2011@gmail.com>
2021-01-17 12:15:04 +0100

    Deleting observer test

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/HomeworkTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/HomeworkTestSuite.java
@@ -1,21 +1,23 @@
 package com.kodilla.patterns2.observer.homework;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class HomeworkTestSuite {
+
+    Student student1 = new Student("Mike");
+    Student student2 = new Student("Jess");
+
+    Homework homework1 = new Homework(20, "some descr");
+    Homework homework2 = new Homework(22, "some descr 22");
+
+    Mentor mentor1 = new Mentor("John");
+    Mentor mentor2 = new Mentor("Rossie");
+    Mentor mentor3 = new Mentor("Sue");
+
     @Test
-    public void testUpdate(){
-        //Given
-        Student student1 = new Student("Mike");
-        Student student2 = new Student("Jess");
-
-        Homework homework1 = new Homework(20,"some descr");
-        Homework homework2 = new Homework(22,"some descr 22");
-
-        Mentor mentor1 = new Mentor("John");
-        Mentor mentor2 = new Mentor("Rossie");
-        Mentor mentor3 = new Mentor("Sue");
+    public void testUpdate() {
 
         //When
         student1.registerObserver(mentor1);
@@ -27,8 +29,27 @@ public class HomeworkTestSuite {
         student2.addNewHomeworkToList(homework2);
 
         //Then
-        assertEquals(1,mentor1.getCount());
-        assertEquals(3,mentor2.getCount());
-        assertEquals(0,mentor3.getCount());
+        assertEquals(1, mentor1.getCount());
+        assertEquals(3, mentor2.getCount());
+        assertEquals(0, mentor3.getCount());
+    }
+
+    @Test
+    public void testDeleteObserver() {
+
+        //Given
+        student1.registerObserver(mentor1);
+        student1.addNewHomeworkToList(homework1);
+        int countBeforeDeletingObserver = mentor1.getCount();
+
+        //When
+        student1.deleteObserver(mentor1);
+        student1.addNewHomeworkToList(homework2);
+        int countAfterDeletingObserver = mentor1.getCount();
+
+        //Then
+        assertEquals(1, countBeforeDeletingObserver);
+        assertEquals(countBeforeDeletingObserver, countAfterDeletingObserver);
+
     }
 }
